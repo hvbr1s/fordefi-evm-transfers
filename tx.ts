05613c05d9394952d9ab1edc6fbe1b334cd96dc7
Dan Jaheny <danjaheny@Dans-MacBook-Pro-2.local>
2025-03-21 11:46:54 +0100

    init

---
+++ tx.ts
@@ -0,0 +1,57 @@
+import dotenv from 'dotenv'
+import { signWithApiSigner } from "./api_request/signer";
+import { createRequest } from "./api_request/form_request";
+import { createAndSignTx } from "./api_request/pushToApi";
+
+////// TO CONFIGURE //////
+dotenv.config()
+const accessToken = process.env.FORDEFI_API_TOKEN;
+const VAULT_ID = process
+
+////// TO CONFIGURE //////
+const fordefiConfig = {
+  accessToken: process.env.FORDEFI_API_USER_TOKEN ?? "",
+  privateKeyPath: "./fordefi_secret/private.pem",
+  vaultId: process.env.VAULT_ID || "",
+  network: "mainnet" as const,
+  senderAddress:process.env.VAULT_ADDRESS || "",
+  pathEndpoint:  "/api/v1/transactions"
+};
+
+const txParams = {
+  evmChain: "arbitrum",
+  to: "0xF659feEE62120Ce669A5C45Eb6616319D552dD93",
+  amount: "10000000"
+}
+
+
+
+async function main(): Promise<void> {
+    if (!accessToken) {
+      console.error('Error: FORDEFI_API_TOKEN environment variable is not set');
+      return;
+    }
+
+    try {
+      // 1. Prepare request body for Fordefi custody service
+      const requestBody = JSON.stringify(await createRequest(fordefiConfig.vaultId, txParams.evmChain, txParams.to, txParams.amount ));
+
+      // 2. Create signature for Fordefi API authentication
+      const timestamp = new Date().getTime();
+      const payload = `${fordefiConfig.pathEndpoint}|${timestamp}|${requestBody}`;
+      const signature = await signWithApiSigner(payload);
+
+      // 3. Submit the transaction to Fordefi API and wait for result
+      const response = await createAndSignTx(fordefiConfig.pathEndpoint, accessToken, signature, timestamp, requestBody);
+      const fordDefiResult = response.data;
+      console.log(fordDefiResult)
+  
+  
+    } catch (error: any) {
+      console.error(`Failed to sign the transaction: ${error.message}`);
+    }
+  }
+  
+  if (require.main === module) {
+    main();
+  }
